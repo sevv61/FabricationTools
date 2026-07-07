b0c74902d75e474af9969793a24f26de44d1d08d
sevv61 <sevv61@gmail.com>
2026-07-07 07:01:42 -0600

    Add GitHub Actions workflows

---
+++ extension/FabricationTools.extension/lib/geometry/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Geometry package.
+
+Provides geometry helper classes used throughout the
+Fabrication Tools extension.
+
+Target:
+    Autodesk Revit 2023
+"""
+
+from .geometry import GeometryInfo
+
+__all__ = [
+    "GeometryInfo",
+]
